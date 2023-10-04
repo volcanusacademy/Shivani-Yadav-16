2182dad4d7b4de384c90f65487d789354558e626
yshivani1212 <yadavshivani395@gmail.com>
2023-10-04 11:10:40 +0530

    add two numbers

---
+++ Pgr_add_two_num.c
@@ -0,0 +1,12 @@
+//Write a program in C to add two numbers.
+
+#include<stdio.h> //// its predefiened input output library
+
+int main() // its a function 
+{
+    int num1 = 30, int2 = 50; // given two numbers are initialized
+
+    printf("Addition of two number is: %d", num1 + num2);
+
+    return 0; //return keyword used for proper initialization
+}
